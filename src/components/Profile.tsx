9404226202f749fcf8aab6e9316c23b441ea7cbb
Hussein Mohamad Hallak <hu100wtf@gmail.com>
2021-02-24 17:26:51 -0400

    first commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,14 @@
+import styles from '../styles/components/Profile.module.css';
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/hmhallak.png" alt="Hussein Hallak"/>
+      <div>
+        <strong>Hussein Hallak</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1</p>
+      </div>
+    </div>
+  )
+}
